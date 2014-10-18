c8cb6843eb5f25737ca0f4e73fe9bfbbc433d504
unknown <okolimar@gmail.com>
2014-10-18 15:22:30 +0300

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,11 +75,11 @@ if ON_PAAS:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',  
-            'NAME':     os.environ['OPENSHIFT_APP_NAME'],
-            'USER':     os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'],
-            'PASSWORD': os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'],
-            'HOST':     os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],
-            'PORT':     os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'],
+            'NAME':     os.environ['decision'],
+            'USER':     os.environ['postgres'],
+            'PASSWORD': os.environ['windowsf'],
+            'HOST':     os.environ['localhost'],
+            'PORT':     os.environ['5432'],
         }
     }
 else:
