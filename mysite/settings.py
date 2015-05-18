6b5992d3b4287e99cbe72161b48a6975d19e6527
Ondrej Kolimar <okolimar@gmail.com>
2015-05-18 18:42:19 +0200

    temporary enabled debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ else:
 # adjust to turn off when on Openshift, but allow an environment variable to override on PAAS
 DEBUG = not ON_PAAS
 DEBUG = DEBUG or 'DEBUG' in os.environ
-
+DEBUG = True
 if ON_PAAS and DEBUG:
     print("*** Warning - Debug mode is on ***")
 
